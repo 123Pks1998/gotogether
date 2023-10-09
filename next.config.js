daf8919b14fb9b664520cc413ad4dd50805e272f
pradeep kumar <105670883+123Pks1998@users.noreply.github.com>
2023-10-10 00:31:15 +0530

    go

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// const nextConfig = {
+//   reactStrictMode: true,
 
-  i18n: {
-    locales: ['en-US', 'hn'],
-    defaultLocale: 'en-US',
-  },
-}
 
-// module.exports = nextConfig
-// module.exports = {
-  
 // }
+
+module.exports = nextConfig
+module.exports = {
+
+}
